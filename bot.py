63854cfdbf78b399ae49a3b559d50b71d420324e
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2024-03-01 13:30:02 +0800

    remove: bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import anonbot
-from anonbot.adapter.satori import Adapter
-
-anonbot.init('config.yml')
-anonbot.get_driver().register_adapter(Adapter)
-anonbot.load_plugins('plugins')
-
-anonbot.run()
